1d2d66f1a6f212d245db2e72727bf835382054d4
Natalie <natalie.sophie.chua@gmail.com>
2024-12-01 18:36:18 +0800

    Combining Day One into one file

--- DayOne/dayone.py
+++ DayOne/dayone.py
@@ -1,6 +1,7 @@
 list1 = []
 list2 = []
 diff = 0
+sim_score = 0
 
 with open("dayone.txt") as text:
     for row in text: 
@@ -12,7 +13,16 @@ with open("dayone.txt") as text:
 sorted_list1 = sorted(list1)
 sorted_list2 = sorted(list2)
 
+# For Difference Score
 for i in range(len(sorted_list1)):
     diff += abs(sorted_list1[i] - sorted_list2[i])
 
+# For Similarity Score
+for i in range(len(list1)):
+    search = list1[i]
+    search_count = list2.count(search)
+    value = search * search_count
+    sim_score += value
+
 print(diff)
+print(sim_score)
